dd63b50d75415d3d721f1fed2a8d65455b0ed3cc
Vitor Pellegrino <vitorp@gmail.com>
2011-05-24 15:21:06 -0300

    Specs refactored

--- spec/sc2_refinery_spec.rb
+++ spec/sc2_refinery_spec.rb
@@ -2,44 +2,44 @@ require 'spec_helper'
 
 describe SC2Refinery do
 
-  context 'when loading a replay'  do
+  context '.load'  do
 
-    it "should parse replays correctly" do
-      validate_parsing("1v1-game1.sc2replay",
-                       :first_player => "pellegrino",
-                       :second_player => "DeusEx",
-                       :map => "Cavernas Xel'Naga")
+    describe 'first replay' do
+      subject { SC2Refinery.load("spec/fixtures/1v1-game1.sc2replay") }
 
-      validate_parsing("1v1-game2.sc2replay",
-                       :first_player => "pellegrino",
-                       :second_player => "Arlequim",
-                       :map => "Vale da Selva")
-
-      validate_parsing("1v1-game3.sc2replay",
-                       :first_player => "pellegrino",
-                       :second_player => "Narsejaum",
-                       :map => "Cavernas Xel'Naga")
-
-      validate_parsing("1v1-game4.sc2replay",
-                       :first_player => "Slaughter",
-                       :second_player => "pellegrino",
-                       :map => "Metal\xC3\xB3polis") # Metalópolis
-      # encoded in ISO-8859-1
-    end
-    it "should parse correctly older version replays" do
-      validate_parsing("1v1-game5-old-version.sc2replay",
-                       :first_player => "WolfMan",
-                       :second_player => "Boom",
-                       :map => "Abyss")
-
+      its(:first_player)  { should == "pellegrino"        }
+      its(:second_player) { should == "DeusEx"            }
+      its(:map)           { should == "Cavernas Xel'Naga" }
     end
 
-    private
-    def validate_parsing(replay_name, params)
-      replay = SC2Refinery.load("spec/fixtures/#{replay_name}")
-      replay.first_player.should == params[:first_player]
-      replay.second_player.should == params[:second_player]
-      replay.map.should == params[:map]
+    describe 'second replay' do
+      subject { SC2Refinery.load("spec/fixtures/1v1-game2.sc2replay") }
+
+      its(:first_player)  { should == "pellegrino"        }
+      its(:second_player) { should == "Arlequim"          }
+      its(:map)           { should == "Vale da Selva"     }
+    end
+    describe 'third replay' do
+      subject { SC2Refinery.load("spec/fixtures/1v1-game3.sc2replay") }
+
+      its(:first_player)  { should == "pellegrino"        }
+      its(:second_player) { should == "Narsejaum"         }
+      its(:map)           { should == "Cavernas Xel'Naga" }
+    end
+    describe 'forth replay' do
+      subject { SC2Refinery.load("spec/fixtures/1v1-game4.sc2replay") }
+
+      its(:first_player)  { should == "Slaughter"          }
+      its(:second_player) { should == "pellegrino"         }
+      # Metalópolis encoded in ISO-8859-1
+      its(:map)           { should == "Metal\xC3\xB3polis" }
+    end
+    context "with older version replays" do
+      subject { SC2Refinery.load("spec/fixtures/1v1-game5-old-version.sc2replay") }
+
+      its(:first_player)  { should == "WolfMan"          }
+      its(:second_player) { should == "Boom"             }
+      its(:map)           { should == "Abyss"            }
     end
   end
 end
