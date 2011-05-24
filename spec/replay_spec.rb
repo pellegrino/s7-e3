e6325d259b218ce6d0310790ccee6f704095c2fd
Vitor Pellegrino <vitorp@gmail.com>
2011-05-24 14:52:39 -0300

    Refactor rspec test

--- spec/replay_spec.rb
+++ spec/replay_spec.rb
@@ -1,21 +1,14 @@
 require 'spec_helper'
 
 describe SC2Refinery::Replay do
-
-  before do
-    @replay =  SC2Refinery::Replay.new(:map => "The Lost Temple", :first_player => "pellegrino", :second_player => "DeusEx")
-  end
-
-  it "should have first player's name" do
-    @replay.first_player.should == "pellegrino"
-  end
-
-  it "should have second player's name" do
-    @replay.second_player.should == "DeusEx"
-  end
-
-  it "should have the map where this match was played"  do
-    @replay.map.should == "The Lost Temple"
-  end
-
+  subject do
+    SC2Refinery::Replay.new(:map => "The Lost Temple",
+                            :first_player => "pellegrino",
+                            :second_player => "DeusEx")
+  end 
+  
+  its(:first_player)  { should == "pellegrino"      } 
+  its(:second_player) { should == "DeusEx"          }
+  its(:map)           { should == "The Lost Temple" }
+  
 end
